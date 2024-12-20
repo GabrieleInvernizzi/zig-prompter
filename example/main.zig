22fb4e0dda8e297e3c38f69b9ccbde0c25beaf4e
GabrieleInvernizzi <gabrieleinvernizzi4@gmail.com>
2024-12-20 10:27:31 +0100

    Improved example.

--- example/main.zig
+++ example/main.zig
@@ -1,30 +1,48 @@
 const std = @import("std");
 const Prompter = @import("prompter");
 
-fn three_len_val(str: []const u8) bool {
+// Example of a simple string validator
+fn len_three_val(str: []const u8) bool {
     return str.len == 3;
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const check = gpa.deinit();
+        if (check == .leak) @panic("The allocator has leaked!");
+    }
     const allocator = gpa.allocator();
-    const stdout_w = std.io.getStdOut().writer();
+    const out = std.io.getStdOut().writer();
 
+    // Initialize the Prompt struct with the default theme
     var p = Prompter.Prompt.init(allocator, Prompter.PromptTheme.default());
 
-    const opts = [_][]const u8{ "Option 1", "Option 2", "Option 3" };
-    const sel_opt = try p.option("Select an option", &opts, 1);
-    if (sel_opt) |o| {
-        try stdout_w.print("\nThe selected option was: {s} (idx: {d})\n", .{ opts[o], o });
-    } else {
-        try stdout_w.writeAll("\nThe selection was aborted.\n");
+    // Try out the option selection prompt
+    {
+        try out.writeAll("[ Option Selection Prompt ]\n");
+        const opts = [_][]const u8{ "Option 1", "Option 2", "Option 3" };
+        const sel_opt = try p.option("Select an option", &opts, 1);
+        if (sel_opt) |o| {
+            try out.print("\nThe selected option was: {s} (idx: {d})\n", .{ opts[o], o });
+        } else {
+            try out.writeAll("\nThe selection was aborted.\n");
+        }
     }
 
-    const input_1 = try p.string("Write something", "Default");
-    defer allocator.free(input_1);
-    try stdout_w.print("{s}\n", .{input_1});
+    // Try out the string prompt
+    {
+        try out.writeAll("\n[ String Prompt ]\n");
+        const input_1 = try p.string("Write something", "Default");
+        defer allocator.free(input_1);
+        try out.print("The input was: {s}\n", .{input_1});
+    }
 
-    const input_2 = try p.stringValidated("Write a string with length = 3", null, three_len_val);
-    defer allocator.free(input_2);
-    try stdout_w.print("{s}\n", .{input_2});
+    // Try out the string prompt with validation (using the function defined above (len_three_val))
+    {
+        try out.writeAll("\n[ Validated String Prompt ]\n");
+        const input_2 = try p.stringValidated("Write a string with length = 3", null, len_three_val);
+        defer allocator.free(input_2);
+        try out.print("The input was: {s}\n", .{input_2});
+    }
 }
